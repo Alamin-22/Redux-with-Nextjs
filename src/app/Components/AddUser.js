2a95c33947542f91e3fffc7ddf7075e490a5775e
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-11 23:19:00 +0600

    install basic redux

--- src/app/Components/AddUser.js
+++ src/app/Components/AddUser.js
@@ -1,13 +1,20 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 
 const AddUser = () => {
+    const [name, setName] = useState("");
+    const userDispatch = (e) => {
+        e.preventDefault();
+        const name = e.target.name
+        console.log(name);
+    }
     return (
         <div className='border p-14'>
-            <h2 className='text-5xl'>User List:</h2>
-            <div className='space-x-1'>
-                <input className='border rounded-lg p-1 border-black' type="text" placeholder='add user' />
-                <input type="button" className='px-3  py-1 bg-red-400 rounded-md text-white' value="Add User" />
-            </div>
+            <h2 className='text-5xl'>Add User :</h2>
+            <form onSubmit={userDispatch} className='space-x-1'>
+                <input onChange={(e) => { setName(e.target.value) }} className='border rounded-lg p-1 border-black' type="text" placeholder='add user' name='name' />
+                <button type='submit' className=' px-3 py-1 bg-red-500 rounded-lg text-white'>Add User</button>
+            </form>
         </div>
     );
 };
